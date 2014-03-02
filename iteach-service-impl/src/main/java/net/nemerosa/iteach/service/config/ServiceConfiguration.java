1b54c626a3fff7f3d3e97fae90ad443041587256
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-02 22:03:47 +0100

    Account service IT - wip

--- iteach-service-impl/src/main/java/net/nemerosa/iteach/service/config/ServiceConfiguration.java
+++ iteach-service-impl/src/main/java/net/nemerosa/iteach/service/config/ServiceConfiguration.java
@@ -1,9 +1,13 @@
 package net.nemerosa.iteach.service.config;
 
+import net.sf.jstring.Strings;
+import net.sf.jstring.support.StringsLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+import java.util.Locale;
 
 @Configuration
 public class ServiceConfiguration {
@@ -11,6 +15,11 @@ public class ServiceConfiguration {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new StandardPasswordEncoder();
+    }
+
+    @Bean
+    public Strings strings() {
+        return StringsLoader.auto(Locale.ENGLISH, Locale.FRENCH, Locale.GERMAN);
     }
 
 }
