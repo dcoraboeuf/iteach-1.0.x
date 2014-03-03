d732b0490864babc2d444feb11ed0990b6c31d3e
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-03 08:28:28 +0100

    Web configuration

---
+++ iteach-ui/src/main/java/net/nemerosa/iteach/ui/config/WebConfig.java
@@ -0,0 +1,36 @@
+package net.nemerosa.iteach.ui.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("net.nemerosa.iteach")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    /**
+     * Uses the HTTP header for content negociation.
+     */
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorPathExtension(false).favorParameter(true);
+    }
+
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        converters.clear();
+        // Plain text
+        converters.add(new StringHttpMessageConverter());
+        // JSON
+        converters.add(new MappingJackson2HttpMessageConverter());
+    }
+
+}
