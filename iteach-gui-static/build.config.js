67e8604851312a664356633628c96b86ceef2ff7
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-10 17:50:03 +0100

    GUI: Using the default routing

--- iteach-gui-static/build.config.js
+++ iteach-gui-static/build.config.js
@@ -60,8 +60,6 @@ module.exports = {
         js: [
             'vendor/angular/angular.js',
             'vendor/angular-bootstrap/ui-bootstrap-tpls.min.js',
-            'vendor/angular-ui-router/release/angular-ui-router.js',
-            'vendor/angular-ui-utils/modules/route/route.js',
             'vendor/angular-translate/angular-translate.js'
         ],
         css: [
