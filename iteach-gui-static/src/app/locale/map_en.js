f7ac709c1816869668a525c57530be7448389bb9
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-10 21:48:06 +0100

    GUI: i18n: localization of the login page

--- iteach-gui-static/src/app/locale/map_en.js
+++ iteach-gui-static/src/app/locale/map_en.js
@@ -1,5 +1,14 @@
 var map_en = {
     'language.en': 'English',
     'language.fr': 'Français',
-    'login.iteach.message': 'iTeach is the application that will help you organise your courses and your planning.'
+    'login.iteach.message': 'iTeach is the application that will help you organise your courses and your planning.',
+    'login.signin.google': 'Sign in with Google',
+    'login.signin.openid': 'Sign in with OpenID',
+    'login.signin.openid.field': 'Enter your OpenID',
+    'login.signin.with': 'Sign in with',
+    'login.signin.form': 'Sign in with iTeach',
+    'login.signin.form.email': 'Enter your email',
+    'login.signin.form.password': 'Enter your password',
+    'login.signin.form.submit': 'Sign in',
+    'login.signin.form.register': 'Register'
 };
--- iteach-gui-static/src/app/locale/map_fr.js
+++ iteach-gui-static/src/app/locale/map_fr.js
@@ -1,5 +1,14 @@
 var map_fr = {
     'language.en': 'English',
     'language.fr': 'Français',
-    'login.iteach.message': "iTeach est l'application qui va vous aider à organiser vos lessons et votre planning."
+    'login.iteach.message': "iTeach est l'application qui va vous aider à organiser vos lessons et votre planning.",
+    'login.signin.google': 'Se connecter avec Google',
+    'login.signin.openid': 'Se connecter avec OpenID',
+    'login.signin.openid.field': 'Entrez votre OpenID',
+    'login.signin.with': 'Se connecter avec',
+    'login.signin.form': 'Se connecter avec iTeach',
+    'login.signin.form.email': 'Entrez votre adresse mail',
+    'login.signin.form.password': 'Entrez votre mot de passe',
+    'login.signin.form.submit': 'Se connecter',
+    'login.signin.form.register': "S'enregistrer"
 };
