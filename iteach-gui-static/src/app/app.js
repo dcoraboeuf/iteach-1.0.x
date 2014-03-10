85e30ce02f57f0737ebf0562c3255795c0c55e7c
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-10 21:06:13 +0100

    GUI: i18n - module

--- iteach-gui-static/src/app/app.js
+++ iteach-gui-static/src/app/app.js
@@ -4,7 +4,7 @@
 var iteach = angular.module('iteach', [
             'ui.bootstrap',
             'ngRoute',
-            // TODO 'pascalprecht.translate',
+            'pascalprecht.translate',
             'iteach.templates',
             'iteach.config',
             // Directives
