830c5b3a9390338f99f28202884ffeced4498793
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-14 21:16:55 +0100

    Form: calling the form

---
+++ iteach-gui-static/src/app/dialog/dialog.form.js
@@ -0,0 +1,17 @@
+angular.module('iteach.dialog.form', [])
+    .service('formService', function ($q, $http, config) {
+
+        var self = {};
+
+        self.load = function (uri) {
+            var d = $q.defer();
+            $http.get(config.api(uri)).success(function (formDefinition) {
+                d.resolve(formDefinition)
+            });
+            return d.promise;
+        }
+
+        return self;
+
+    })
+;
--- iteach-gui-static/src/app/dialog/dialog.school.js
+++ iteach-gui-static/src/app/dialog/dialog.school.js
@@ -1,5 +1,9 @@
-angular.module('iteach.dialog.school', [])
-    .controller('dialogSchool', function ($scope, $modalInstance) {
+angular.module('iteach.dialog.school', [
+        'iteach.dialog.form'
+    ])
+    .controller('dialogSchool', function ($scope, $modalInstance, formService) {
+
+        $scope.form = formService.load('teacher/school/form');
 
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel')
