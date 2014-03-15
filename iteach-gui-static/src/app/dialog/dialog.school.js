895fe1cc323476b4fd9fb4ffb0441fbb9ab0005d
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-15 21:31:01 +0100

    GUI: Form validation: colour

--- iteach-gui-static/src/app/dialog/dialog.school.js
+++ iteach-gui-static/src/app/dialog/dialog.school.js
@@ -1,6 +1,7 @@
 angular.module('iteach.dialog.school', [])
     .controller('dialogSchool', function ($log, $scope, $modalInstance) {
 
+        $scope.colourPattern = /#[0-9A-Fa-f]{6}/;
         $scope.hourlyRatePattern = /^([A-Z]{3} )?\d+(\.\d+)?$/;
         $scope.school = {};
 
