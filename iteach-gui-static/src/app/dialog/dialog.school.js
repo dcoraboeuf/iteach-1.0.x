dd597e19e8b58aa1de96f864264b32ce6bcf48a5
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-15 14:44:43 +0100

    Form: correct binding by using an object (see https://github.com/angular/angular.js/wiki/Understanding-Scopes) - no directive yet

--- iteach-gui-static/src/app/dialog/dialog.school.js
+++ iteach-gui-static/src/app/dialog/dialog.school.js
@@ -1,14 +1,16 @@
 angular.module('iteach.dialog.school', [])
     .controller('dialogSchool', function ($log, $scope, $modalInstance) {
 
-        $scope.name = 'test';
+        $scope.school = {
+            name: 'test'
+        };
 
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel')
         }
 
         $scope.submit = function () {
-            $log.debug('name=', $scope.name);
+            $log.debug('name=', $scope.school.name);
             // $modalInstance.close('ok')
         }
 
