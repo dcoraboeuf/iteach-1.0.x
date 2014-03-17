255035c87ead35db05adc8457676046d562ef5c0
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-17 09:27:19 +0100

    UC: create a school: hourly rate without currency

--- iteach-ui-model/src/main/java/net/nemerosa/iteach/ui/model/UISchoolForm.java
+++ iteach-ui-model/src/main/java/net/nemerosa/iteach/ui/model/UISchoolForm.java
@@ -1,36 +1,45 @@
 package net.nemerosa.iteach.ui.model;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.experimental.Builder;
 import org.apache.commons.lang3.StringUtils;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.URL;
+import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
 
 @Data
+@Builder
+@AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class UISchoolForm {
 
     @NotNull
-    @Size(min=1, max = 80)
+    @Size(min = 1, max = 80)
     private final String name;
     @Pattern(regexp = "#[0-9A-Fa-f]{6}")
     private final String colour;
-    @Size(min=0, max = 80)
+    @Size(min = 0, max = 80)
     private final String contact;
-    @Size(min=0, max = 20)
+    @Size(min = 0, max = 20)
     @Pattern(regexp = "^(([A-Z]{3} )?\\d+(\\.\\d+)?)?$")
     private final String hourlyRate;
-    @Size(min=0, max = 200)
+    @Size(min = 0, max = 200)
     private final String postalAddress;
-    @Size(min=0, max = 40)
+    @Size(min = 0, max = 40)
     private final String phone;
-    @Size(min=0, max = 40)
+    @Size(min = 0, max = 40)
     private final String mobilePhone;
-    @Size(min=0, max = 120)
+    @Size(min = 0, max = 120)
     @Email
     private final String email;
-    @Size(min=0, max = 200)
+    @Size(min = 0, max = 200)
     @URL
     private final String webSite;
 
@@ -46,8 +55,14 @@ public class UISchoolForm {
      */
 
     public Money toHourlyRate() {
-        return StringUtils.isNotBlank(hourlyRate)
-                ? Money.parse(hourlyRate)
-                : null;
+        if (StringUtils.isNotBlank(hourlyRate)) {
+            if (StringUtils.containsOnly(hourlyRate, "0123456789.")) {
+                return Money.of(CurrencyUnit.EUR, new BigDecimal(hourlyRate));
+            } else {
+                return Money.parse(hourlyRate);
+            }
+        } else {
+            return null;
+        }
     }
 }
