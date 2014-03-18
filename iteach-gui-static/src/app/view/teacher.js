ab07a9f7d1d37074ef52424858ad9f576b8092ff
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-03-18 21:05:36 +0100

    Planning: Empty calendar

--- iteach-gui-static/src/app/view/teacher.js
+++ iteach-gui-static/src/app/view/teacher.js
@@ -39,6 +39,24 @@ angular.module('iteach.view.teacher', [
             teacherService.createStudent().then(loadStudents)
         }
 
+        /**
+         * Planning
+         */
+        $scope.calendarConfig = {
+            calendar:{
+                height: 450,
+                editable: true,
+                header:{
+                    left: 'month basicWeek basicDay agendaWeek agendaDay',
+                    center: 'title',
+                    right: 'today prev,next'
+                },
+                dayClick: $scope.alertEventOnClick,
+                eventDrop: $scope.alertOnDrop,
+                eventResize: $scope.alertOnResize
+            }
+        };
+        $scope.lessons = [];
 
         // TODO Loads the planning
     })
