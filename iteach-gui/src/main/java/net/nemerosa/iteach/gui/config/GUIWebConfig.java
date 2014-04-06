b70eb69792ae60bf727f886fa4546f406c6d5d77
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-04-06 11:06:45 +0200

    GUI layer: moved the static resource handler to the GUI layer

--- iteach-gui/src/main/java/net/nemerosa/iteach/gui/config/GUIWebConfig.java
+++ iteach-gui/src/main/java/net/nemerosa/iteach/gui/config/GUIWebConfig.java
@@ -2,8 +2,22 @@ package net.nemerosa.iteach.gui.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan("net.nemerosa.iteach")
-public class GUIWebConfig {
+public class GUIWebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // FIXME Setup depends on the profile, dev or prod
+        registry.addResourceHandler("/app/**").addResourceLocations("/app/");
+        registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
+        registry.addResourceHandler("/vendor/**").addResourceLocations("/vendor/");
+        registry.addResourceHandler("index.html").addResourceLocations("index.html");
+    }
+
 }
--- iteach-ui/src/main/java/net/nemerosa/iteach/ui/config/WebConfig.java
+++ iteach-ui/src/main/java/net/nemerosa/iteach/ui/config/WebConfig.java
@@ -12,7 +12,6 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
@@ -39,15 +38,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setObjectMapper(ObjectMapperFactory.create());
         converters.add(converter);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // TODO Setup depends on the profile, dev or prod
-        registry.addResourceHandler("/app/**").addResourceLocations("/app/");
-        registry.addResourceHandler("/assets/**").addResourceLocations("/assets/");
-        registry.addResourceHandler("/vendor/**").addResourceLocations("/vendor/");
-        registry.addResourceHandler("index.html").addResourceLocations("index.html");
     }
 
     // Message source
