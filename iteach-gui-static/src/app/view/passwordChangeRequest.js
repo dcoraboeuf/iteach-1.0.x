c37dff0117968cfbe9b87d552cb1b980947c8ed4
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-04-06 10:27:22 +0200

    UC: Password change: correct routing according to the logging status

--- iteach-gui-static/src/app/view/passwordChangeRequest.js
+++ iteach-gui-static/src/app/view/passwordChangeRequest.js
@@ -2,7 +2,7 @@ angular.module('iteach.view.passwordChangeRequest', [
         'iteach.service.account',
         'iteach.service.core'
     ])
-    .controller('PasswordChangeRequestCtrl', function ($scope, $routeParams, $translate, accountService, routeService) {
+    .controller('PasswordChangeRequestCtrl', function ($scope, $routeParams, $translate, $location, accountService, routeService) {
         $scope.oldPassword = '';
         $scope.newPassword = '';
         $scope.newPasswordConfirm = '';
@@ -12,7 +12,11 @@ angular.module('iteach.view.passwordChangeRequest', [
             // Sending the form
             accountService.passwordChange($routeParams.token, $scope.oldPassword, $scope.newPassword)
                 .success(function () {
-                    routeService.routeWithSuccess('/teacher', $translate.instant('account.passwordChange.success'))
+                    if ($scope.logged()) {
+                        routeService.routeWithSuccess('/teacher', $translate.instant('account.passwordChange.success'))
+                    } else {
+                        routeService.routeWithSuccess('/login', $translate.instant('account.passwordChange.success'))
+                    }
                 })
         }
     })
