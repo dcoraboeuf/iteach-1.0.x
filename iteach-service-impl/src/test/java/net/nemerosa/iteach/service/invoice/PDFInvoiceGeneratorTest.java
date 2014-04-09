50967574edd0e75644be02a15dc5ff3cba96af15
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2014-04-09 09:24:34 +0200

    Invoice mgt: complete test data

--- iteach-service-impl/src/test/java/net/nemerosa/iteach/service/invoice/PDFInvoiceGeneratorTest.java
+++ iteach-service-impl/src/test/java/net/nemerosa/iteach/service/invoice/PDFInvoiceGeneratorTest.java
@@ -1,8 +1,6 @@
 package net.nemerosa.iteach.service.invoice;
 
-import net.nemerosa.iteach.service.model.InvoiceData;
-import net.nemerosa.iteach.service.model.Profile;
-import net.nemerosa.iteach.service.model.School;
+import net.nemerosa.iteach.service.model.*;
 import org.apache.commons.io.FileUtils;
 import org.joda.money.CurrencyUnit;
 import org.joda.money.Money;
@@ -13,6 +11,7 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.YearMonth;
+import java.util.Arrays;
 import java.util.Locale;
 
 public class PDFInvoiceGeneratorTest {
@@ -20,16 +19,40 @@ public class PDFInvoiceGeneratorTest {
     @Test
     public void generate() throws IOException {
         // Sample data
+        final Money hourlyRate = Money.of(CurrencyUnit.EUR, 20);
+        final int teacherId = 1;
+        final int schoolId = 1;
+        final String schoolColour = "#FF0000";
+        final String schoolName = "School";
         InvoiceData data = new InvoiceData(
                 YearMonth.of(2014, 4),
                 LocalDate.now(),
                 2014001L,
-                1, "Teacher Name", "teacher@test.com",
+                teacherId, "Teacher Name", "teacher@test.com",
                 new Profile("Company", "", "Rue de la Ville\n1000 Brussels", "01234", "BE56789", "BE567", "BBGGRR", 1L),
-                new School(1, 1, "School", "#FF0000", "", Money.of(CurrencyUnit.EUR, 56), "Avenue du Village\n3000 Labas", "", "", "", "", "BE09876", BigDecimal.valueOf(21)),
-                null,
-                null,
-                null
+                new School(schoolId, teacherId, schoolName, schoolColour, "", hourlyRate, "Avenue du Village\n3000 Labas", "", "", "", "", "BE09876", BigDecimal.valueOf(21)),
+                new SchoolReport(
+                        schoolId,
+                        schoolName,
+                        schoolColour,
+                        hourlyRate,
+                        BigDecimal.valueOf(10),
+                        Money.of(CurrencyUnit.EUR, 200),
+                        Arrays.asList(
+                                new StudentReport(
+                                        1, false, "Student 1", "Subject 1",
+                                        BigDecimal.valueOf(6),
+                                        Money.of(CurrencyUnit.EUR, 120)
+                                ),
+                                new StudentReport(
+                                        2, false, "Student 2", "Subject 2",
+                                        BigDecimal.valueOf(4),
+                                        Money.of(CurrencyUnit.EUR, 80)
+                                )
+                        )
+                ),
+                Money.of(CurrencyUnit.EUR, 42.0),
+                Money.of(CurrencyUnit.EUR, 242.0)
         );
         // Generation
         PDFInvoiceGenerator generator = new PDFInvoiceGenerator();
