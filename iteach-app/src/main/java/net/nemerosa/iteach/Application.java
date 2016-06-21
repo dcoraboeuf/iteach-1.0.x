de8d69b52b3079c8e9c604d682d94d39c46a2560
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-06-21 20:25:23 +0200

    OAuth - conditional login

--- iteach-app/src/main/java/net/nemerosa/iteach/Application.java
+++ iteach-app/src/main/java/net/nemerosa/iteach/Application.java
@@ -3,13 +3,29 @@ package net.nemerosa.iteach;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class Application {
+public class Application extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http
+                .antMatcher("/**")
+                .authorizeRequests()
+                    .antMatchers("/", "/login**", "/webjars/**")
+                    .permitAll()
+                .anyRequest()
+                    .authenticated()
+        ;
+        // @formatter:on
     }
 
 }
---
+++ iteach-app/src/main/java/net/nemerosa/iteach/controller/UserController.java
@@ -0,0 +1,17 @@
+package net.nemerosa.iteach.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+public class UserController {
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        // TODO Returns a specific class
+        return principal;
+    }
+
+}
